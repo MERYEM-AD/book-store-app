5c0b54ed1e82050f25d60006f1774722e0c407d2
MERYEM-AD <adiri.meryem.mw@gmail.com>
2022-03-02 21:38:58 -0500

    Pushing front end

---
+++ client/src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import SearchBooks from './SearchBooks';
+import {Container} from 'react-bootstrap';
+
+function Home () {
+
+        return (
+          
+  
+  <Container>
+      {/* <SearchBooks /> */}
+    
+
+
+ 
+
+
+
+
+</Container>
+
+
+
+        )
+    
+}
+export default  Home;
