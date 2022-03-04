c38616c9b9a8ed6f147b94c4723dfba034951d4e
MERYEM-AD <adiri.meryem.mw@gmail.com>
2022-03-03 19:38:07 -0500

    Search bar done

---
+++ client/src/pages/Reviews.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Card} from 'react-bootstrap'
+import { useQuery } from '@apollo/client';
+
+
+ function Reviews () {
+        return (
+            <div>
+<h1>
+    Display all User's Reviews
+</h1>
+
+            </div>
+        )
+
+}
+
+export default Reviews;
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -16,7 +16,7 @@ import Auth from '../utils/auth';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(QUERY_ME);
-  // eslint-disable-next-line 
+ 
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
   const userData = data?.me || {};
@@ -31,7 +31,7 @@ const SavedBooks = () => {
     }
 
     try {
-      // eslint-disable-next-line 
+      
       const { data } = await removeBook({
         variables: { bookId },
       });
